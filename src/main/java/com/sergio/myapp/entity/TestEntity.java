471a9189115e780a7ba3bd21c595e154bbeb56e7
seraph8712 <sergioave.87@gmail.com>
2021-08-29 21:14:09 -0500

    Nombre del proyecto: myapp, descripcion: version final del proyecto myapp

---
+++ src/main/java/com/sergio/myapp/entity/TestEntity.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.sergio.myapp.entity;
+
+/**
+ * @author Sergio Avendano
+ *
+ */
+public class TestEntity {
+	/*
+	 * Propiedad de prueba para la muestra de compilación del archivo
+	 * testentity
+	 */
+	
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+
+}
